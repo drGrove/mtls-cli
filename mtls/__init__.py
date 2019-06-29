da216be5c57eff7a76a0cb03ccac1eb3ea392096
Danny Grove <danny@drgrovellc.com>
2019-06-28 20:15:30 -0700

    Always build the binaries so they can be manually tested if needed

--- mtls/__init__.py
+++ mtls/__init__.py
@@ -5,13 +5,13 @@ from pkg_resources import get_distribution, DistributionNotFound
 
 __author__ = "Danny Grove <danny@drgrovellc.com>"
 
-# Allows "import mtls" and "from mtls import <name>".
-sys.path.extend([os.path.join(os.path.dirname(__file__), "..")])
-
 try:
     __version__ = get_distribution(__name__).version
 except DistributionNotFound:
     __version__ = "dev"
+
+# Allows "import mtls" and "from mtls import <name>".
+sys.path.extend([os.path.join(os.path.dirname(__file__), "..")])
 
 from .cli import cli  # noqa
 from .mtls import MutualTLS  # noqa
